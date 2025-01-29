68d4fa114d336a2a087699fe14eebef4b94fb400
Saideep9835 <tsaideep98@gmail.com>
2025-01-29 08:21:47 -0600

    Initial Commit

---
+++ OpenAiProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenAiProject
+//
+//  Created by Saideep Reddy Talusani on 1/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenAiProject/OpenAiProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAiProjectApp.swift
+//  OpenAiProject
+//
+//  Created by Saideep Reddy Talusani on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct OpenAiProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
